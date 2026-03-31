ca28250b4fc5db674d073e5569be268948cfbe67
VasudevaSurf <vasu@teams.surf.mt>
2026-04-01 00:06:24 +0530

    added url

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -56,7 +56,7 @@ export const config: EnvConfig = {
   cors: {
     allowedOrigins: getEnvVarOptional(
       "ALLOWED_ORIGINS",
-      "http://localhost:3000,http://localhost:5173,https://surf-seller-page-admin-panel.vercel.app,https://surf-seller-page.vercel.app,https://sell.surf.mt"
+      "http://localhost:3000,http://localhost:5173,https://surf-seller-page-admin-panel.vercel.app,https://surf-seller-page.vercel.app,https://sell.surf.mt,https://surf-seller-page-kappa.vercel.app"
     )
       .split(",")
       .map((origin) => origin.trim()),
